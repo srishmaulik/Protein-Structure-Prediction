f5a571b3cf46bc317674924d0038cc13f350adf2
srishmaulik <srishmaulik2004@gmail.com>
2025-01-14 19:16:19 -0800

    protein prediction and requirements

---
+++ app/main.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, HTTPException
+from dotenv import load_dotenv
+import os
+import requests
+import py3Dmol
+from stmol import showmol
+
+load_dotenv()
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    app_name = os.getenv("APP_NAME", "Protein Predicter")
+    return {"message": f"Welcome to {app_name}!"}
+
+
+
+
+def rendermol(pdb):
+    pdbview = py3Dmol.view()
+    pdbview.addModel(pdb, 'pdb')
+    pdbview.setStyle({'cartoon':{'color':'spectrum'}})
+    pdbview.setBackGroundColor('white')
+    pdbview.zoomTo()
+    pdbview.zoom(2, 800)
+    pdbview.spin(True)
+    showmol(pdbview, height=500, width=800)
+
+
+@app.get("/getprotein/{uniprot_id}")
+def predict_protein(uniprot_id: str):
+    data = ""
+    try:
+        api_endpoint = "https://alphafold.ebi.ac.uk/api/prediction/"
+        url = f"{api_endpoint}{uniprot_id}"
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            protein_data =  response.json()
+        else:
+            raise HTTPException(status_code=404, detail="Protein Structure not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error") from e
+    pdb_url = protein_data[0].get('pdbUrl')
+    if pdb_url:
+        pdb_data = requests.get(pdb_url).text
+    else:
+        raise HTTPException(status_code=404, detail="PDB URL not found for the protein")
+
+
+    return {"pdb_data": pdb_data, "protein_data": protein_data}
+
+
+        
+
+    
+   
+    
+
+
+
+
+
+
+    
+
